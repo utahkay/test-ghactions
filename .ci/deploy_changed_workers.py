57b98dfc7f20c9f8af348579a3681534b5702e88
Kay Johansen <kay.johansen@streamnative.io>
2022-12-18 21:05:56 -0700

    Put the changes in a map

--- .ci/deploy_changed_workers.py
+++ .ci/deploy_changed_workers.py
@@ -7,3 +7,10 @@ print(all_changes)
 
 worker_changes = [x.removeprefix("workers/") for x in all_changes.split() if x.startswith("workers/")]
 print(worker_changes)
+w = []
+for change in worker_changes:
+    i = change.find('/')
+    if i != -1:
+        w[change.prefix(i)] = change.suffix(change.length-i)
+
+print(w)
