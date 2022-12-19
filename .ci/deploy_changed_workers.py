815bb61a6f6bb8c1a567929810952da8af7f066e
Kay Johansen <kay.johansen@streamnative.io>
2022-12-18 21:00:01 -0700

    Update python script to split string

--- .ci/deploy_changed_workers.py
+++ .ci/deploy_changed_workers.py
@@ -1,6 +1,9 @@
 import os
 
 print("Files changed:")
-changed=os.environ.get("FILES_CHANGED")
-print(type(changed))
-print(changed)
+all_changes=os.environ.get("FILES_CHANGED")
+print(type(all_changes))
+print(all_changes)
+
+worker_changes = [x.removeprefix("workers/") for x in all_changes.split() if x.startswith("workers/")]
+print(worker_changes)
